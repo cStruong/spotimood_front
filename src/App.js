473ebde93ecf369520b66cd1409eac1ab5c8cd57
cstruong <christophertruongm@gmail.com>
2019-05-05 19:20:37 -0400

    first commit basic SPA skeleton

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PlaylistContainer from './Components/PlaylistContainer'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="header">
+          HEADER/LOGO BAR THING FOR LOGIN AND STUFF
+        </div>
+
+        <div className="appcolumn right"> 
+        SEARCH BAR PLAYLIST CONTAINER, MOODPLATLIST CONTAINER GOES HERE
+        <PlaylistContainer />
+        </div>
+        
+        <div className="appcolumn left">
+          <div className="profilepic">
+              PROFILE PICTURE BOX
+          </div>
+          
+        PROFILE AND SUBMITTING NEW SONG GOES HERE
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/PlaylistContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class PlaylistContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PlaylistContainer;
